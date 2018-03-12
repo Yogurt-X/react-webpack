37bd21d4c5cdadf70e6e6ad93daa5116f382ac06
lubambi <lubambi420@163.com>
2018-03-12 17:04:21 +0800

    路由配置

--- client/app.js
+++ client/app.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
 // react-hot-loader的热更新是依赖于webpack-dev-server(打包文件改变时reload刷新整个页面)
 // react-hot-loader会根据stateNode节点的更新对比，只更新改变的reactDom节点
 // 从而保留了未改变的state值，更适用于react的开发更新模式
 // 更新1：使用AppContainer去包裹我们的根节点想要渲染的实际HTML内容
 import { AppContainer } from 'react-hot-loader' // eslint-disable-line
-import App from './App.jsx'
+import App from './views/App'
 
 // ReactDOM.hydrate(<App />, document.getElementById('root'))
 // 更新2：封装成可传递参数的方法
@@ -16,7 +17,9 @@ const root = document.getElementById('root')
 const render = (Component) => {
   ReactDOM.hydrate(
     <AppContainer>
-      <Component />
+      <BrowserRouter>
+        <Component />
+      </BrowserRouter>
     </AppContainer>,
     root,
   )
@@ -25,8 +28,8 @@ const render = (Component) => {
 render(App);
 
 if (module.hot) {
-  module.hot.accept('./App.jsx', () => {
-    const NextApp = require('./App.jsx').default //eslint-disable-line
+  module.hot.accept('./views/App', () => {
+    const NextApp = require('./views/App').default //eslint-disable-line
     // ReactDOM.hydrate(<NextApp />, document.getElementById('root'))
     render(NextApp)
   })
--- client/server-entry.js
+++ client/server-entry.js
@@ -1,7 +1,7 @@
 // 用到了JSX就需要import 'react'
 import React from 'react'
 // 'APP'会到node_modules目录下面找
-import App from './App.jsx'
+import App from './views/App'
 
 // 将需要服务端渲染的内容export出去
 export default <App />
